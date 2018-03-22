6d3923d5eb8ac082a501e5800198f84eee5d790c
Shota Kawashima <bysk.333@gmail.com>
2018-03-22 17:10:32 +0900

    fixめう

--- bin/index.js
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env node
-console.log(process.argv[process.argv.length - 1] + 'めう！')
--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-module.exports = require('./bin')
+#!/usr/bin/env node
+console.log(process.argv[process.argv.length - 1] + 'めう！')

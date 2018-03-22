2113ab844c700a3314ba76ec6ccee5c3023703c1
Shota Kawashima <bysk.333@gmail.com>
2018-03-22 18:23:09 +0900

    fix

--- cli.js
+++ cli.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 const meu = require('./index.js')
-for(let i = 1;i < process.argv.length;i++){
+for(let i = 2;i < process.argv.length;i++){
   console.log(meu(process.argv[i]))
 }

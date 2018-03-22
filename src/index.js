765f00621d50fd369b1fb041976e4874d62f5fb0
Shota Kawashima <bysk.333@gmail.com>
2018-03-22 21:21:29 +0900

    fix bug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 function meu(str){
   const lines = str.split('\n')
-  if (lines.length === 1) if (str != '') return str + 'めう'
-  else{
+  if (lines.length === 1){
+    if (str != '') return str + 'めう'
+  }else{
     return lines.reduce((cur, prev, i) => {
       if(i === 1) {
         if (prev === '') return cur + 'めう\n'
